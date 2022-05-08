197044952998fe9de07b9a126656cf11718a0482
OWAIS14325 <owaissiddiquione@gmail.com>
2022-05-08 11:53:18 +0530

    fixed responsive issue

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -227,7 +227,7 @@
         <div class="pb-16 bg-gradient-to-r from-amber-500 to-orange-600 lg:pb-0 lg:z-10 lg:relative">
           <div class="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-3 lg:gap-8">
             <div class="relative lg:-my-8">
-              <div aria-hidden="true" class="absolute inset-x-0 top-0 h-1/2 bg-white lg:hidden" />
+              <div aria-hidden="true" class="absolute inset-x-0 top-0 lg:h-1/2 bg-white lg:hidden" />
               <div class="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:p-0 lg:h-full">
                 <div class="aspect-w-10 aspect-h-6 rounded-xl shadow-xl overflow-hidden sm:aspect-w-16 sm:aspect-h-7 lg:aspect-none lg:h-full">
                   <img class="object-cover lg:h-full lg:w-full" src="https://images.unsplash.com/photo-1520333789090-1afc82db536a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />
